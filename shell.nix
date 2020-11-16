3bf898834dea9bce7794b1afa6632de4ea500810
Joe Betz <joebetz91@gmail.com>
2020-11-16 04:27:51 +0100

    conditionally add ethereum packages

--- shell.nix
+++ shell.nix
@@ -1,9 +1,14 @@
+{ ethereum ? false }:
 let
   pkgs = import ./pkgs.nix;
   glow = import ./default.nix;
+  ethereumPkgs = if ethereum then [ pkgs.go-ethereum ] else [];
 in
   pkgs.mkShell {
     inputsFrom = [
       glow
     ];
+    buildInputs = ethereumPkgs;
+    shellHook = glow.postConfigure;
+    GERBIL_APPLICATION_HOME = "./";
   }
