39299adbd29bb81e932eeb68dc97408dc19095c2
AlexKnauth <alexander@knauth.org>
2021-01-31 15:11:31 -0500

    scribble stand-alone, not racket-package-docs

--- info.rkt
+++ info.rkt
@@ -6,12 +6,19 @@
 ;; Package Info
 
 (define collection "glow")
-(define deps '("base"))
-(define build-deps '("scribble-lib"))
+(define deps '("base" "scribble-lib"))
 
 ;; ---------------------------------------------------------
 ;; Collection Info
 
-(define scribblings '(("scribblings/glow.scrbl" (multi-page))))
 (define compile-omit-paths 'all)
-(define compile-include-files '("main.rkt" "scribblings/glow.scrbl"))
+(define compile-include-files
+  '("main.rkt"
+    "scribblings/glow-code.rkt"
+    "scribblings/glow-explanation.scrbl"
+    "scribblings/glow-how-to.scrbl"
+    "scribblings/glow-parse.rkt"
+    "scribblings/glow-reference-manual.scrbl"
+    "scribblings/glow-surface-grammar.scrbl"
+    "scribblings/glow-tutorial.scrbl"
+    "scribblings/glow.scrbl"))
