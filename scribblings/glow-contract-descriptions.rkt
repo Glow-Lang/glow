978e76c0bd2eda439fe51a0ab65380954bf34e01
Nate Homesley <nehomesley@gmail.com>
2021-02-24 15:59:59 +0000

    More review updates

--- scribblings/glow-contract-descriptions.rkt
+++ scribblings/glow-contract-descriptions.rkt
@@ -13,11 +13,10 @@ Currently @(Glow) supports three example DApps.
 
 @bold{Buy Signature Agreement}
 
-A buy-sig agreement is an interaction between two parties, where the DApp 
-specifies the sale price and the hash of the document to be signed. When the
-seller provides the correct signature proving that they have signed the correct
-document, the DApp releases moves the price amount from the buyer, to the
-seller.
+A buy-sig agreement is an interaction between two parties, where the DApp specifies
+the sale price and the hash of the document to be signed. When the seller provides
+the correct signature proving that they have signed the correct document, the DApp
+transfers the assets previously deposited by the Buyer to the Seller. 
 
 Parameters
 @itemize[
@@ -29,28 +28,32 @@ Parameters
 
 @bold{Coin Flip Game}
 
-A coin flip game is an interaction between two parties, where the DApp
-specifies the wager amount and calculates the winner based on integers chosen
-by each player. When both parties provide their choice, called a "hand", the
-DApp computes the winner and sends the wager to that party.
+A coin flip game is an interaction between two parties, where the DApp specifies
+the wager amount and calculates the winner based on integers chosen by each player.
+When both parties provide their choice (in the form of an integer) the DApp computes
+the winner and sends the wager to that party. This DApp illustrates how untrusting
+participants can generate a number mutually trusted to be random. The uses for this
+protocol are far ranging in a wide number of situations, and the bet on whether the
+number is odd is only meant for illustration, being the simplest way to use the
+randomness to control the distribution of digital assets.
 
 Parameters
 @itemize[
     @item{Player 1 Address}
-    @item{Player 1 Hand}
+    @item{Player 1 Integer Choice}
     @item{Player 2 Address}
-    @item{Player 2 Hand. This is provided after the DApp is deployed.}
+    @item{Player 2 Integer Choice. This is provided after the DApp is deployed.}
     @item{Wager. This amount will be sent from the loser to the winner after execution.}
 ]
 
 @bold{Rock Paper Scissors Game}
 
-A rock-paper-scissors game is an interaction between two parties, where the
- DApp specifies the wager amount and calculates the winner based on simple
- rules (Rock > Scissors, Scissors > Paper, Paper > Rock). When both parties
- provide their choice, called a "hand", the DApp computes the winner and
- sends the wager to that party. In the event of a tie, the interaction is
- complete and the wager does not exchange between the players.
+A rock-paper-scissors game is an interaction between two parties, where the DApp
+specifies the wager amount and calculates the winner based on simple rules
+(Rock > Scissors, Scissors > Paper, Paper > Rock). When both parties provide their
+choice, called a "hand", the DApp computes the winner and sends the wager to that
+party. In the event of a tie, the interaction is complete and the wager does not
+exchange between the players.
 
 Parameters
 @itemize[
